0d3ac960a82a2a59f4f2499aeeab6af367aa8a50
tstevens <tstevens@me.com>
2009-11-25 13:38:30 -0500

    Moved body tag onload and onunload function calls to application.js to clean up partial rendering.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,10 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+//These replace onload and onunload functions in body tag.
+window.onload = function() {
+   init();
+}
+window.onunload = function(){
+	GUnload();
+}
