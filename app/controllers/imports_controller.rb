87dea827542fc91cc357017406dd6ff600b177ca
tstevens <tstevens@me.com>
2009-11-25 16:17:02 -0500

    Added other columns to Flights table. Cleaned up migration files.

--- app/controllers/imports_controller.rb
+++ app/controllers/imports_controller.rb
@@ -63,15 +63,27 @@ private
     params = Hash.new
       params[:flight] = Hash.new
       params[:flight]["title"] = line[0]
-  	  params[:flight]["time"] = line[1]
+      params[:flight]["time"] = line[1]
+      params[:flight]["point_source"] = line[2]
       params[:flight]["latitude"] = line[3]
       params[:flight]["longitude"] = line[4]
-  	  params[:flight]["altitude"] = line[5]
+      params[:flight]["altitude"] = line[5]
+      params[:flight]["altDot"] = line[6]
       params[:flight]["groundspeed"] = line[11]
       params[:flight]["heading"] = line[12]
-  	  params[:flight]["air_temp"] = line[7]
+      params[:flight]["air_temp"] = line[7]
       params[:flight]["air_density"] = line[8]
       params[:flight]["pressure"] = line[9]
+      params[:flight]["delta_distance"] = line[10]
+      params[:flight]["windDir"] = line[11]
+      params[:flight]["windMag"] = line[12]
+      params[:flight]["true_airspeedCalc"] = line[13]
+      params[:flight]["true_airspeedUsed"] = line[14]
+      params[:flight]["true_airspeed"] = line[15]
+      params[:flight]["true_airspeed_dot"] = line[16]
+      params[:flight]["bank_angle"] = line[17]
+      params[:flight]["config"] = line[18]
+      params[:flight]["lift_coeff"] = line[19]
       flight = Flight.new(params[:flight])
     flight.save
 	end
--- db/migrate/20091123204343_create_flights.rb
+++ db/migrate/20091123204343_create_flights.rb
@@ -2,15 +2,27 @@ class CreateFlights < ActiveRecord::Migration
   def self.up
     create_table :flights do |t|
       t.string :title
-  	  t.integer :time
-  	  t.float :latitude
-  	  t.float :longitude
-  	  t.float :altitude
-  	  t.float :groundspeed
-  	  t.float :heading
-  	  t.float :air_temp
-  	  t.float :air_density
-  	  t.float :pressure
+      t.integer :time
+      t.float :latitude
+      t.float :longitude
+      t.float :altitude
+      t.float :groundspeed
+      t.float :heading
+      t.float :air_temp
+      t.float :air_density
+      t.float :pressure
+      t.string :point_source
+      t.float :altDot
+      t.float :delta_distance
+      t.float :windDir
+      t.float :windMag
+      t.float :true_airspeedCalc
+      t.float :true_airspeedUsed
+      t.float :true_airspeed
+      t.float :true_airspeed_dot
+      t.float :bank_angle
+      t.string :config
+      t.float :lift_coeff
         t.timestamps
     end
   end
