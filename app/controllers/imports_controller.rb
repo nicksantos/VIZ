98836644298d332c08ccaf1cb50d0167f80d91cd
tstevens <tstevens@me.com>
2009-12-02 15:46:11 -0500

    Cleaned up views and fixed syntax. Added layout for imports.

--- app/controllers/imports_controller.rb
+++ app/controllers/imports_controller.rb
@@ -45,6 +45,7 @@ class ImportsController < ApplicationController
       end
       @import.save
       flash[:notice] = "CSV data processing was successful."
+
       redirect_to :action => "show", :id => @import.id
     else
       flash[:error] = "CSV data processing failed."
